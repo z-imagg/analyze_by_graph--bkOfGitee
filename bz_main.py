9e97f85f4ae34998f5d48bc072ba1c5466ad452e
prgrmz07 <prgrmz07@163.com>
2024-04-02 23:08:43 +0800

    业务函数 草稿

--- bz_main.py
+++ bz_main.py
@@ -15,22 +15,35 @@ from traverse import TraverseAbs
 from neo4j_tool import neo4j_update
 from print_nowDateTime_with_prevSeconds_tool import nowDateTimeTxt
 
+def lsIsEmpty(ls:typing.List[typing.Any])->bool:
+    empty:bool = ls is None or len(ls) == 0
+    return empty
+
 class BzDeepth(TraverseAbs):
     def __init__(self, sess: Session) -> None:
         super().__init__(sess)
 
 # 【业务函数】 计算深度
     def bz(self, RE:Node, RL:Node, isLeaf:bool, S:typing.List[int], _)->int:
+
+        #断言叶子的直接孩子们为空，目的是 检验本项目的其他地方逻辑是否有问题
+        sonLs:typing.List[Node] = _
+        if isLeaf:
+            assert lsIsEmpty(sonLs) 
+
         #deepth数值列表即为S
         deepth_ls:typing.List[int]= S
 
+        #断言起点、终点fnCallId相同，目的是 检验本项目的其他地方逻辑是否有问题
         E_fnCallId=RE['fnCallId']
         L_fnCallId=RL['fnCallId']
         assert E_fnCallId == L_fnCallId
 
-        if isLeaf: return 0
-        else:
-            return 1+max(deepth_ls)
+        #叶子的deepth为0,非叶子的deepth为 1+直接孩子们的deepth的最大值
+        d=0 if isLeaf  else 1+max(deepth_ls)
+
+        #注意此返回是必须的, 否则 遍历器traverse.py.TraverseAbs.V中的'S=[...bz()...]'将得不到返回值
+        return d
 
 class BzWriteDeepth(TraverseAbs):
 
@@ -41,30 +54,57 @@ class BzWriteDeepth(TraverseAbs):
 
 # 【业务函数】 计算深度 并 写deepth字段为深度值
     def bz(self, RE:Node, RL:Node, isLeaf:bool, S:typing.List[int], _)->int:
+
+        #断言叶子的直接孩子们为空，目的是 检验本项目的其他地方逻辑是否有问题
+        sonLs:typing.List[Node] = _
+        if isLeaf:
+            assert lsIsEmpty(sonLs)
+    
         #deepth数值列表即为S
         deepth_ls:typing.List[int]= S
 
-
+        #断言起点、终点fnCallId相同，目的是 检验本项目的其他地方逻辑是否有问题
         E_fnCallId=RE['fnCallId']
         L_fnCallId=RL['fnCallId']
         assert E_fnCallId == L_fnCallId
-
         fnCallId=E_fnCallId
-
         
+        #叶子的deepth为0,非叶子的deepth为 1+直接孩子们的deepth的最大值
         d=0 if isLeaf  else 1+max(deepth_ls)
         neo4j_update(sess,"update_setFieldDeepth",BzWriteDeepth.cypher__update_setFieldDeepth,params={"prm_fnCallId":fnCallId,"prm_deepth":d},filedName="更新记录数")
         print(f"BzWriteDeepth.bz, {nowDateTimeTxt()}, fnCallId={fnCallId}写字段deepth={d}; 第{self.Vi}次遍历")
         
+        #注意此返回是必须的, 否则 遍历器traverse.py.TraverseAbs.V中的'S=[...bz()...]'将得不到返回值
         return d
 
 class BzWriteWidth(TraverseAbs):
+    cypher__update_setFieldWidth=readTxt("cypher_src/update_setFieldWidth.cypher") 
+
     def __init__(self, sess: Session) -> None:
         super().__init__(sess)
 
     def bz(self, RE:Node, RL:Node, isLeaf:bool, _, C:typing.List[Node])->Node:
-        pass
 
+        #断言叶子的直接孩子们为空，目的是 检验本项目的其他地方逻辑是否有问题
+        sonLs:typing.List[Node] = C
+        if isLeaf:
+            assert lsIsEmpty(sonLs) 
+
+        #断言起点、终点fnCallId相同，目的是 检验本项目的其他地方逻辑是否有问题
+        E_fnCallId=RE['fnCallId']
+        L_fnCallId=RL['fnCallId']
+        assert E_fnCallId == L_fnCallId
+        fnCallId=E_fnCallId
+
+        #叶子的width为0,非叶子的width为直接孩子个数
+        width=0 if isLeaf  else len(sonLs)
+        #写width字段
+        neo4j_update(sess,"update_setFieldWidth",BzWriteWidth.cypher__update_setFieldWidth,params={"prm_fnCallId":fnCallId,"prm_width":width},filedName="更新记录数")
+        print(f"BzWriteWidth.bz, {nowDateTimeTxt()}, fnCallId={fnCallId}写字段width={width}; 第{self.Vi}次遍历")
+
+        #注意此返回是必须的, 否则 遍历器traverse.py.TraverseAbs.V中的'S=[...bz()...]'将得不到返回值
+        return None
+    
 class 址数:
     def __init__(self,fnAdr:str,cnt:int) -> None:
         self.址:str=fnAdr
@@ -89,7 +129,6 @@ class 成份:
         新.内容.extend(dct.items())
         return 新
 
-    
 
 class BzWrite成份(TraverseAbs):
     def __init__(self, sess: Session) -> None:
@@ -104,12 +143,16 @@ class BzWrite成份(TraverseAbs):
 
         fnAdr=E_fnAdr
 
+        markup=None
         if isLeaf:
             #不写成份字段
-            return 成份( ).填1个(fnAdr)
+            markup= 成份( ).填1个(fnAdr)
         else:
             #写成份字段
-            return 成份( ).merge(*S)
+            markup= 成份( ).merge(*S)
+        
+        #注意此返回是必须的, 否则 遍历器traverse.py.TraverseAbs.V中的'S=[...bz()...]'将得不到返回值
+        return markup
 
 
 
---
+++ cypher_src/update_setFieldWidth.cypher
@@ -0,0 +1,8 @@
+// 对给定fnCallId的起点、终点， 新增width字段并赋值为参数$prm_width
+// 【术语】prm == param
+with
+$prm_width as prmWidth ,
+$prm_fnCallId as prmFnCallId 
+MATCH (fromLog:V_FnCallLog {fnCallId:prmFnCallId} )-[:E_FnEL]->(toLog:V_FnCallLog  {fnCallId:prmFnCallId} )
+SET fromLog.width = prmWidth, toLog.width = prmWidth 
+return count(fromLog)+count(toLog)   as 更新记录数
