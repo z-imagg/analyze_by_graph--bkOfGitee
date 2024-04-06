6156d8fa35875fb2a32488d792762f05972ec957
prgrmz07 <prgrmz07@163.com>
2024-04-06 11:16:29 +0800

    基本工具函数

--- fridaLog-sqlite3-neo4j/basic_tool.py
+++ fridaLog-sqlite3-neo4j/basic_tool.py
@@ -5,3 +5,17 @@
 #【术语】 
 #【备注】 
 #【术语】 
+
+from sqlite3 import Row as sqlite3Row
+import typing
+
+## 基本工具函数
+
+### 列表判空
+def lsIsEmpty(ls:typing.List[typing.Any]) -> bool:
+    return ls is None or len(ls) == 0
+
+### join 整数们
+def joinInts(_intLs:typing.List[int],_sep:str=",")->str:
+    _strLs=[f"{k}" for k in _intLs]
+    return _sep.join(_strLs)
