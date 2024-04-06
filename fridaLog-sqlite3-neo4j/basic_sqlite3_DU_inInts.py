476bd2542b967dd82e5e0f089435dc5b96af1eea
prgrmz07 <prgrmz07@163.com>
2024-04-06 11:25:44 +0800

    sqlite删改函数

---
+++ fridaLog-sqlite3-neo4j/basic_sqlite3_DU_inInts.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#【标题】 sqlite删改函数
+#【术语】 
+#【备注】 
+#【术语】 
+
+from sqlite3 import Row as sqlite3Row
+import typing
+
+from basic_sqlite3_func import sq3DU
+from basic_tool import joinInts
+## sqlite删改函数
+
+### sq3DU_inInts: sqlite3执行sql删改 携带in整数列表条件  
+def sq3DU_inInts(sq3dbConn,sqlInIntLs,intLs):
+    intStrLs=joinInts(intLs)
+    sqlTxt=sqlInIntLs.format(lsVar=intStrLs)
+    return sq3DU(sq3dbConn,sqlTxt)
+
+#调用举例
+# sq3DU_inInts(sq3dbConn,"select  *  from t_FnCallLog where fnCallId in ({lsVar}) ",[-1,-20]    )
+# -1
+
