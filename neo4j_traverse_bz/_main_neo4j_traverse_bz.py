05e6ba1219cfcc4434495d5b148501e2e3918ca6
prgrmz07 <prgrmz07@163.com>
2024-04-06 23:01:33 +0800

    注释

--- neo4j_traverse_bz/_main_neo4j_traverse_bz.py
+++ neo4j_traverse_bz/_main_neo4j_traverse_bz.py
@@ -8,26 +8,30 @@ from neo4j_CAUD_chain import Neo4j_CAUD_chain
 
 dbConn_inject_neo4j_default(
 lambda sess: 
-#删除全部 V_chain
+    #删除全部 V_chain
     Neo4j_CAUD_chain._del_chain(sess)
 )
 
 dbConn_inject_neo4j_default(
 lambda sess: 
+    #遍历孤立群 做BzDeepth业务， 多个遍历
     LoopTraverse(trav=BzDeepth(sess)).loop_traverse()
 )
 
 dbConn_inject_neo4j_default(
 lambda sess: 
+    #遍历孤立群 做BzWriteDeepth业务， 多个遍历
     LoopTraverse(trav=BzWriteDeepth(sess)).loop_traverse()
 )
 
 dbConn_inject_neo4j_default(
 lambda sess: 
+    #遍历孤立群 做BzWriteWidth业务， 多个遍历
     LoopTraverse(trav=BzWriteWidth(sess)).loop_traverse()
 )
 
 dbConn_inject_neo4j_default(
 lambda sess: 
+    #遍历孤立群 做BzWriteMarkup业务， 多个遍历
     LoopTraverse(trav=BzWriteMarkup(sess)).loop_traverse()
 )
