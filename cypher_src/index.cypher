dcc45023cbab0331ca7a14a0080d73a7c3f4152a
prgrmz07 <prgrmz07@163.com>
2024-05-03 19:31:43 +0800

    遍历器开始前 创建索引

--- cypher_src/index.cypher
+++ cypher_src/index.cypher
@@ -1,9 +1,6 @@
 
 // CREATE INDEX FOR (n:V_FnCallLog) ON (n.logId)  // .ipynb中创建的索引
-
 CREATE INDEX FOR (n:V_FnCallLog) ON (n.deepth) ;
 CREATE INDEX FOR (n:V_FnCallLog) ON (n.fnCallId) ;
-
 CREATE INDEX FOR (n:V_FnCallLog) ON (n.fnAdr) ;
-
 CREATE INDEX FOR (n:V_FnCallLog) ON (n.fnAdr,n.direct) ;
--- neo4j_traverse_bz/_main_neo4j_traverse_bz.py
+++ neo4j_traverse_bz/_main_neo4j_traverse_bz.py
@@ -5,6 +5,16 @@ from bz_markup_write_main import BzWriteMarkup
 from bz_width_write_main import BzWriteWidth
 from dbConn_inject_neo4j import dbConn_inject_neo4j_default
 from neo4j_CAUD_chain import Neo4j_CAUD_chain
+from neo4j_index_constraint import neo4j_recreateIdx
+from util_file import readTxt
+
+
+cypher__index_create=readTxt("cypher_src/index.cypher") 
+dbConn_inject_neo4j_default(
+lambda sess: 
+    #遍历器开始前 创建索引
+    neo4j_recreateIdx(sess,cypher__index_create)
+)
 
 dbConn_inject_neo4j_default(
 lambda sess: 
