92173e4b36efb91b31c515380352cc7439fdec83
prgrmz07 <prgrmz07@163.com>
2024-05-10 11:31:41 +0800

    配合frida_js的修改'目前日志文件软链接'

--- fridaLog-sqlite3-neo4j/fridaLog_fullPath_get.py
+++ fridaLog-sqlite3-neo4j/fridaLog_fullPath_get.py
@@ -14,13 +14,14 @@ import typing
 
 from pathlib import Path
 def getLogFullPath()->str:
-    logF_ls=[ * Path("/fridaAnlzAp/frida_js/").glob("./frida-out-Pure-*.log") ]
-    assert len(logF_ls)==1
+    #frida_js/_main.sh中有如下定义 :
+    #  目前日志文件软链接
+    _LogFP_PureNow_link="/gain/frida-out/PureNow.log"
 
+    # 【目前日志文件软链接举例】 /gain/frida-out/PureNow.log ---> /gain/frida-out/torch/Pure-1712031317.log
 
-    # 日志文件全路径举例: TorchFnCallLogFP="/fridaAnlzAp/frida_js/frida-out-Pure-1712031317.log"
-
-
-
-    TorchFnCallLogFP=logF_ls[0].as_posix()
-    return TorchFnCallLogFP
+    logFp=Path(_LogFP_PureNow_link)
+    assert logFp.exists(), f"断言失败 目前日志文件软链接 不存在. _LogFP_PureNow_link={_LogFP_PureNow_link}"
+    
+    logFpTxt=logFp.as_posix()
+    return logFpTxt
