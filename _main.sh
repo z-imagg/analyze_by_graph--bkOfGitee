333180638799d3bd90a5f23d2200e87be8c9c384
prgrmz07 <prgrmz07@163.com>
2024-05-10 22:49:22 +0800

    [4] fix: loop_traverse边界条件？:'set -o pipefail': 管道不吃错误代码

--- _main.sh
+++ _main.sh
@@ -41,6 +41,7 @@ export PYTHONPATH="$_PyDirCommon:/fridaAnlzAp/analyze_by_graph/_sqlite3/:/fridaA
 
 #fridaLog转sqlite3转neo4j
 _errMsg1="_main_fridaLog-sqlite3-neo4j.py报错，请解决后，重新执行此脚本analyze_by_graph/_main.sh,退出代码"
+#                                               'set -o pipefail': 管道后的tee不吃错误代码 
 python fridaLog-sqlite3-neo4j/_main_fridaLog-sqlite3-neo4j.py 2>&1 | tee fridaLog-sqlite3-neo4j-${now}.log || { _exitCode1=$? ; echo "${_errMsg1} ${_exitCode1}" ; exit $_exitCode1 ;}
 
 read -p "fridaLog转sqlite3转neo4j 已执行完毕，按回车则将执行遍历器:"
