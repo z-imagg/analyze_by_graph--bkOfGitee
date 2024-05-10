e8c1ee3b2aca751a66ccae6e12ae2a98fe0c6269
prgrmz07 <prgrmz07@163.com>
2024-05-10 12:25:52 +0800

    [3] 配合frida_js的修改'目前日志文件软链接'

--- config.py
+++ config.py
@@ -7,12 +7,15 @@
 
 
 from neo4j_db_basic import Neo4J_DB_Entity
+from util_path import followFilePathSoftLink_getDirName_assert
 
 #frida_js/_main.sh中有如下定义 :
 #  目前日志文件软链接
 # _LogFP_PureNow_link="/gain/frida-out/PureNow.log"
-# 【目前日志文件软链接举例】 /gain/frida-out/PureNow.log ---> /gain/frida-out/torch/Pure-1712031317.log
+# 【目前日志文件软链接举例】 /gain/frida-out/PureNow.log ---> /gain/frida-out/appName--qemu-system-x86_64--v8.2.2/Pure-1712031317.log
 FnCallLogFP="/gain/frida-out/PureNow.log"
+#  举例 appName=='qemu-system-x86_64--v8.2.2'
+appName=followFilePathSoftLink_getDirName_assert(FnCallLogFP,"appName--")
 
 sqlite3_dbFilePath='/gain/analyze_by_graph-out/FnCallLog.db'
 neo4jDB_default= Neo4J_DB_Entity(URI="neo4j://localhost:7687", AUTH_user="neo4j", AUTH_pass="123456", DB_NAME="neo4j")
--- util/util_path.py
+++ util/util_path.py
@@ -3,7 +3,7 @@
 
 #【标题】 路径工具函数
 #【术语】 
-#【备注】 
+#【备注】 此文不能依赖上层业务脚本，否则可能循环依赖
 #【术语】 
 
 
@@ -15,3 +15,16 @@ def assertFilePathExisted(fPTxt:str,errMsg:str)->str:
     logFp=Path(fPTxt)
     assert logFp.exists(), f"【断言失败 路径不存在】【errMsg='{errMsg}'】 【fPTxt={fPTxt}】"
     return   fPTxt
+
+#解析给定路径的实际指向，获得该实际指向的目录名, 并断言该目录名含有assertWord
+def followFilePathSoftLink_getDirName_assert(fPTxt:str,assertWord:str)->str:
+    fP=Path(fPTxt)
+    targetFP=fP.resolve().absolute()
+    _dirName:str=targetFP.parent.name
+    
+    #断言 目录名包含 assertWord
+    assert assertWord is not None and _dirName.__contains__(assertWord)
+    
+    dirName=_dirName.replace(_dirName,assertWord)
+    
+    return   dirName
